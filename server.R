e60f29a443f42e039956405164feacc9cf88038d
Ekram Milash <ekram@Ekrams-MacBook-Pro.local>
2024-05-29 16:07:03 -0700

    finished interactive page #1

--- server.R
+++ server.R
@@ -1,4 +1,37 @@
 library(shiny)
-server <- function(input, output, session) {
+library(plotly)
+library(ggplot2)
+library(bslib)
+data <- read.csv("~/exploratory-analysis-eam52/Survey on Student Mental Health1.csv")
+server <- function(input, output) {
+  output$my_chart <- renderPlotly({
+    if (input$mentalHealth == "Screen Time"){
+      plot_data <- ggplot(data, aes(x = On_what_do_you_spend_your_screen_time_more, fill =  On_what_do_you_spend_your_screen_time_more)) +
+        geom_bar() +
+        labs(title = "Screen Time Usage", x = "Activities", y = "Count", fill = "Screen Time Activities")
+      ggplotly(plot_data)
+    } else if (input$mentalHealth == "Diet"){
+      plot_data <- ggplot(data, aes(x = Are_you_getting_good_food_diet_everyday, fill = Are_you_getting_good_food_diet_everyday)) +
+        geom_bar() +
+        labs(title = "Dietary Habits", x = "Are you getting good food diet everyday?", y = "Count", fill = "Diet Status") +
+        theme_minimal()
+      
+      ggplotly(plot_data, tooltip = c("x", "y"))
+    } else if(input$mentalHealth == "Stress") {
+      plot_data <- ggplot(data, aes(x = When_you_are_stressed_more, fill = When_you_are_stressed_more)) +
+        geom_bar() +
+        labs(title = "Causes of Stress in College Students", x = "When", y = "Causes", fill = "When stressed") +
+        theme_minimal()
+      ggplotly(plot_data, tooltip = c("x", "y"))
+    } else {
+      NULL
+    }
+
+  })
   
 }
+
+
+  
+
+
--- ui.R
+++ ui.R
@@ -1,7 +1,8 @@
 library(shiny)
 library(bslib)
 library(rsconnect)
-
+library(plotly)
+library(ggplot2)
 ui <- navbarPage(
   title = "INFO 201 App",
   theme = bs_theme(),
@@ -49,15 +50,83 @@ ui <- navbarPage(
     )
   ),
   tabPanel(
-    title = "Interactive Page 1",
+    title = "Negative Factors",
     fluidPage(
       tags$head(
         tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
       ),
-      h1("Interactive Page 1", class = "main-title text-center mt-4"),
-      p("The chart attempts to answer the question: “How do dietary habits correlate with mental health conditions among college students?”", class = "section-content"),
-      p("We understand that diet can affect our daily routines, whether we eat too little or too much, both of which can be caused by stress. Students respond to stress differently, and understanding that, we wanted to research the relationship between dietary habits, performance, and mental health among college students.", class = "section-content")
-
+      h1("Contributing Factors of Mental Health on College Students", class = "main-title text-center mt-4"),
+      sidebarLayout(
+        sidebarPanel(
+          selectInput("mentalHealth", "Different variables that affect mental Health:",
+                      choices = c("Screen Time", "Stress", "Diet"))
+        ),
+        mainPanel(
+          fluidRow(
+            column(width = 12,
+                   conditionalPanel(
+                     condition = "input.mentalHealth == 'Screen Time'",
+                     h3("The Purpose Of The Chart", class="section-title"),
+                     p("The chart attempts to answer the question: “What do you spend your screen time on?”", class = "section-content"),
+                     p("We understand that the time spent on screen time is critical to one's mental health. Students respond to stress differently, and understanding that, we wanted to research the relationship between screen time activities and mental health among college students.", class = "section-content"),
+                     p("as we can see majority of the students spend their time on social media, which can cause mental Health coditions such as: depression, anxiety, and stress. ", class = "section-content"),
+                     h3("Key Insights and findings", class = "section-title"),
+                     tags$ul(
+                       tags$li("Majority of student spend thier screen time on Social Media"),
+                       tags$li("Others spend it on Studying"),
+                       tags$li("and the rest saud others which includs video games, movies and more"),
+                     ), 
+                     h3("Resources", class="section-title"),
+                     p("Read more about effects of Screen Time on college Students", class ="section-content"),
+                     p(tags$a(href = "https://www.honorsociety.org/articles/impact-screen-time-college-students-finding-balance-digital-age#:~:text=Mental%20Health%20Challenges%3A%20Heavy%20screen,portray%20an%20idealized%20online%20persona.", "Impacts of Screen Time on College Students", class = "section-content")),
+                     p(tags$a(href = "https://www.opb.org/article/2023/02/27/screen-time-body-image/s", "Limiting Screen Time", class = "section-content")),
+                     p(tags$a(href = "https://www.thesantaclara.org/blog/college-students-are-as-bad-as-ipad-kids", "College Students Vs Ipad Kids", class = "section-content"))
+                     
+                   ),
+                   conditionalPanel(
+                     condition = "input.mentalHealth == 'Diet'",
+                     h3("The Purpose Of The Chart", class="section-title"),
+                     p("This part of the section answers the question: Are you getting good food diet everyday", class = "section-content"),
+                     p("Poor Diet has many drawbacks when it coms to not only mentalhealth but also overal health, if you are in college, you tend to east less than the average person due to stress, workoverload, poor time managment and more...", class = "section-content"),
+                     h3("Key Insights and findings", class = "section-title"),
+                     tags$ul(
+                       tags$li("More than half of the students surveyed said NO"),
+                       tags$li("Very little amount of people said YES")
+                     ),
+                     p("What this lets us know is that majority of student are not properly feeding themselves, which can cause major mental health comditons.", class ="section-content"),
+                     h3("Resources", class="section-title"),
+                     p("Read more about effects of diet in our daily activities, mental health and overal health", class ="section-content"),
+                     p(tags$a(href = "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6659154/", "Mental Health & Nutrition", class = "section-content")),
+                     p(tags$a(href = "https://www.healthline.com/nutrition/healthy-eating-tips", "Healthy Meal Plans for College Students", class = "section-content")),
+                     p(tags$a(href = "https://www.medicalnewstoday.com/articles/322268", "Strategies on Improving Your Eating Habits", class = "section-content"))
+                   ),
+                   conditionalPanel(
+                     condition = "input.mentalHealth == 'Stress'",
+                     h3("The Purpose Of The Chart", class = "section-title"),
+                     p("According to National Institutes of Health (NIH):", class = "section-content"), 
+                     tags$a(href = "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9169886/", "Link to the NIH Website", class = "section-content"),
+                     p("Academic stress is one of the dominant stress factors among college students, and it is said to affect the mental well-being of students, especially those in harder fields of study. Just like stated, some groups have a higher chance of stress due to different factors.", class = "section-content"),
+                     p("This Stress Chart attempts to answer the question: When are you stressed the most? Asking students to respond with the most cause of stress factor they experience.", class = "section-content"),
+                     h3("Key Insights and findings", class = "section-title"),
+                     p("From the chart below, we learn the main cause of stress among college students:", class = "section-content"),
+                     tags$ul(
+                     tags$li("Majority of student stress from exams"),
+                     tags$li("The next group is stress about money"),
+                     tags$li("followed by, not getting enought sleep"),
+                     ),
+                     h3("Resources", class="section-title"),
+                     p("Read more about effects of stress and how to cope with stress", class ="section-content"),
+                     p(tags$a(href = "https://www.purdueglobal.edu/blog/student-life/college-students-guide-to-stress-management-infographic/", "Stress Managments", class = "section-content")),
+                     p(tags$a(href = "https://jedfoundation.org/resource/understanding-academic-stress/", "Stress effects on college students", class = "section-content")),
+                     p(tags$a(href = "https://learningcenter.unc.edu/tips-and-tools/managing-college-stress/", "How to cope with stress as a college students", class = "section-content"))
+                   ),
+                   column(width = 12,
+                          plotlyOutput(outputId = "my_chart")
+                   )
+            )
+          )
+        )
+      )
     )
   ),
   tabPanel(
@@ -78,6 +147,7 @@ ui <- navbarPage(
       ),
       h1("Interactive Page 3", class = "main-title text-center mt-4"),
       p("")
+      
     )
   ),
   tabPanel(
