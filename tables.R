4d7856f23e517e8964576dfb38ee461050cca073
Ekram <128111692+eam52@users.noreply.github.com>
2024-05-29 20:09:46 -0700

    Update tables.R

--- tables.R
+++ tables.R
@@ -5,19 +5,17 @@ data_set <- read.csv("~/Documents/exploratory-analysis-eam52/Survey on Student M
 
 mutate_col_names <- data_set %>%
   mutate(
-    screen = On_what_do_you_spend_your_screen_time_more ,
+    screen = On_what_do_you_spend_your_screen_time_more,
     stress = When_you_are_stressed_more,
-    diet = Are_you_getting_good_food_diet_everyday 
+    diet = Are_you_getting_good_food_diet_everyday
   )
 
 # Group by major and reason for the choosing of the major
-agregated_data_frame <- mutate_col_names %>%
-  group_by(major, reason, attitude, sleep_amount, diet) %>%
-  summarize(
-    average_gpa = mean(gpa, na.rm = TRUE),
-    maximum_gpa = max(gpa, na.rm = TRUE),
-    median_gpa = median(gpa, na.rm = TRUE),
-    total_students = n()
-  )
+aggregated_data_frame <- mutate_col_names %>%
+  count(screen, stress, diet, name = "Total_students_reported")
 
-print(agregated_data_frame, n = nrow(agregated_data_frame))
+output$chart1 <- renderTable({
+  aggregated_data_frame
+})
+
+print(aggregated_data_frame, n = nrow(aggregated_data_frame),  na.print = "NA")
